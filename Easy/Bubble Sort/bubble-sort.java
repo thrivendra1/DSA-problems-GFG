bbd83464f586f3637545c9815e67c4177483fb09
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-02-28 16:05:16 +0530

    Updated Solution - GfG to GitHub

--- Easy/Bubble Sort/bubble-sort.java
+++ Easy/Bubble Sort/bubble-sort.java
@@ -14,15 +14,15 @@ class Solution
 	public static void bubbleSort(int arr[], int n)
     {
         //code here
-        for(int i=0;i<n;i++)
+        for(int i=0;i<arr.length-1;i++)
         {
-            for(int j=1;j<n;j++)
+            for(int j=0;j<arr.length-i-1;j++)
             {
-                if(arr[j-1]>arr[j])
+                if(arr[j]>arr[j+1])
                 {
-                    int temp=arr[j-1];
-                    arr[j-1]=arr[j];
-                    arr[j]=temp;
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
         }
