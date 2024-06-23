21f96d000405e389b95c615318581389fc2fbb95
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-06-23 18:36:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.java
@@ -38,14 +38,68 @@ public class Main {
 class Solution {
     int count(int[] arr, int n, int x) {
         // code here
-        int count=0;
-        for(int i=0;i<n;i++)
+        // int count=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(arr[i]==x)
+        //     {
+        //         count++;
+        //     }
+        // }
+        // return count;
+        int f=first(arr,n,x);
+        int l=last(arr,n,x);
+        if(l==-1) return 0;
+        int ans=l-f+1;
+        return ans;
+    }
+    public int first(int []arr,int n,int x)
+    {
+        int low=0;
+        int high=n-1;
+        int idx=-1;
+        while(low<=high)
         {
-            if(arr[i]==x)
+            int mid=(low+high)/2;
+            if(arr[mid]==x)
             {
-                count++;
+                idx=mid;
+                high=mid-1;
+            }
+           else if(x<arr[mid])
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
             }
         }
-        return count;
+        return idx;
     }
+    
+        public int last(int []arr,int n,int x)
+    {
+        int low=0;
+        int high=n-1;
+        int idx=-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]==x)
+            {
+                idx=mid;
+                low=mid+1;
+            }
+           else if(x<arr[mid])
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return idx;
+}
 }
